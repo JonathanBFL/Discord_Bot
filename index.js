60a034b0bc146622f7adc219344cf7cc147251dc
JonathanBFL <masterchiefjoncomcast@gmail.com>
2021-09-20 17:22:08 -0400

    v1.0.1

--- index.js
+++ index.js
@@ -48,37 +48,3 @@ client.on('interactionCreate', async interaction => {
 // Login to Discord with your client's token
 client.login(token);
 
-
-
-
-
-
-
-
-
-
-
-// Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
-
-// Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
-
-//shows the bot is running in console
-client.once('ready', () => {
-    console.log('Ready!');
-});
-
-client.on('interactionCreate', async interaction => {
-    if (!interaction.isCommand()) return; //returns if no command
-
-    const { commandName } = interaction; //declaring what to call
-
-    if (commandName === 'join') { //looks for the command 'join'
-        await interaction.reply(`Joined: ${interaction.member.joinedAt}`); //prints when user joined
-    }
-});
-
-// Login to Discord with token
-client.login(token);
