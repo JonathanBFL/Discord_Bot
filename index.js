8d698968368719c0d6d63e08988a5653b303ae6e
JonathanBFL <masterchiefjoncomcast@gmail.com>
2021-09-21 00:47:52 -0400

    v1.0.2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // Require the necessary discord.js classes
-const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const {Client, Intents} = require('discord.js');
+const {token} = require('./config.json');
 const moment = require("moment");
 
 // Create a new client instance
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({intents: [Intents.FLAGS.GUILDS]});
 
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
@@ -14,69 +14,88 @@ client.once('ready', () => {
 
 });
 
-client.on('interactionCreate', async interaction => {
+//Function for how long members have been active
+function TimeConv() {
 
-    //returns if no command
-    if (!interaction.isCommand()) return;
+    client.on('interactionCreate', interaction => {
 
-    //declaring what to call
-    const { commandName } = interaction;
+        //returns if no command
+        if (!interaction.isCommand()) return;
 
-    //message created timestamp - unix timecode
-    const createdtimestamp = (interaction.createdTimestamp);
+        //declaring what to call
+        const {commandName} = interaction;
 
-    //author join-date timestamp - unix timecode
-    const joinedtimestamp = (interaction.member.joinedTimestamp);
+        //message created timestamp - unix timecode
+        const createdtimestamp = (interaction.createdTimestamp);
 
-    //convert unix timecode into hours.
-    // /1000 to convert to seconds. /60 converts to minutes. /60 converts to hours. rounds down.
-    const totaltimehour = Math.floor((createdtimestamp - joinedtimestamp) / 1000 / 60 / 60);
+        //author join-date timestamp - unix timecode
+        const joinedtimestamp = (interaction.member.joinedTimestamp);
 
-    // /24 converts hours to days - rounds down.
-    const totaltimeday = Math.floor(totaltimehour / 24);
+        //formatted time when user joined
+        const joinedformated = moment.utc(interaction.member.joinedAt);
 
-    // Subtract total number days off total number of hours
-    //prevents out of bounds var if days/weeks/months/years are used
-    const adjustedhours = totaltimehour - (totaltimeday * 24);
+        //convert unix timecode into hours.
+        // /1000 to convert to seconds. /60 converts to minutes. /60 converts to hours. rounds down.
+        const totaltimehour = Math.floor((createdtimestamp - joinedtimestamp) / 1000 / 60 / 60);
+        console.log(totaltimehour);
 
-    //Months uses 30 days as a month todo: fix month usage
-    const totaltimemonth = Math.floor(totaltimeday / 30);
+        // /24 converts hours to days - rounds down.
+        const totaltimeday = Math.floor(totaltimehour / 24);
+        console.log(totaltimeday);
 
-    //takes total days subtracted by (num. of months * 30)
-    //prevents out of bounds var if weeks/months/years are used
-    const adjusteddays = totaltimeday - (totaltimemonth * 30);
+        // Subtract total number days off total number of hours
+        //prevents out of bounds var if days/weeks/months/years are used
+        const adjustedhours = totaltimehour - (totaltimeday * 24);
+        console.log(adjustedhours);
 
-    //takes total days divided by 365 to get years
-    const totaltimeyear = Math.floor(totaltimeday / 365);
+        //Months uses 30 days as a month todo: fix month usage
+        const totaltimemonth = Math.floor(totaltimeday / 30);
+        console.log(totaltimemonth);
 
-    //prevents out of bounds var if years are used
-    const adjustedmonths = totaltimemonth - (totaltimeyear * 12);
+        //takes total days subtracted by (num. of months * 30)
+        //prevents out of bounds var if weeks/months/years are used
+        const adjusteddays = totaltimeday - (totaltimemonth * 30);
+        console.log(adjusteddays);
 
-    //looks for the command 'join'
-    if (commandName === 'join') {
+        //takes total days divided by 365 to get years
+        const totaltimeyear = Math.floor(totaltimeday / 365);
+        console.log(totaltimeyear);
 
-        //looks for length of time if 1year+
-        if (totaltimeyear > 0){
-            await interaction.reply(`Joined: ${moment.utc(interaction.member.joinedAt)}\nMember for ${totaltimeyear} years ${adjustedmonths} months ${adjusteddays} days ${adjustedhours} hours`);
+        //prevents out of bounds var if years are used
+        const adjustedmonths = totaltimemonth - (totaltimeyear * 12);
+        console.log(adjustedmonths);
+
+        //looks for the command 'join'
+        if (commandName === 'join') {
+
+            //looks for length of time if 1year+
+            if (totaltimeyear > 0) {
+                interaction.channel.send(`Joined: ${joinedformated}\nMember for ${totaltimeyear} years ${adjustedmonths} months ${adjusteddays} days ${adjustedhours} hours`);
+            }
+
+            //looks for length of time if 1month+
+            else if (totaltimemonth > 0) {
+                interaction.channel.send(`Joined: ${joinedformated}\nMember for ${totaltimemonth} months ${adjusteddays} days ${adjustedhours} hours`)
+            }
+
+            //looks for length of time if 1day+
+            else if (totaltimeday > 0) {
+                interaction.channel.send(`Joined: ${joinedformated}\nMember for ${totaltimeday} days ${adjustedhours} hours`);
+            }
+
+            //looks for length of time if 1hour+
+            else if (totaltimehour > 0) {
+                interaction.channel.send(`Joined: ${joinedformated}\nMember for ${totaltimehour} hours`);
+            } else {
+                interaction.channel.send(`Joined: ${joinedformated}`);
+            }
         }
 
-        //looks for length of time if 1month+
-        else if (totaltimemonth > 0){
-            await interaction.reply(`Joined: ${moment.utc(interaction.member.joinedAt)}\nMember for ${totaltimemonth} months ${adjusteddays} days ${adjustedhours} hours`);
-        }
+    });
+}
 
-        //looks for length of time if 1day+
-        else if (totaltimeday > 0){
-            await interaction.reply(`Joined: ${moment.utc(interaction.member.joinedAt)}\nMember for ${totaltimeday} days ${adjustedhours} hours`);
-        }
-
-        //looks for length of time if 1hour+
-        else if (totaltimehour > 0){
-            await interaction.reply(`Joined: ${moment.utc(interaction.member.joinedAt)}\nMember for ${totaltimehour} hours`);
-        }
-
-    }
-});
+//calls TimeConv function
+TimeConv();
 
 // Login to Discord with your client's token
 client.login(token);
